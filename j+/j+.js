d60965e908d9a5a62bc22c4775a6cffa058fbafd
zhwq <zhngwing@163.com>
2012-03-22 18:08:52 +0800

    j+ init

---
+++ j+/j+.js
@@ -0,0 +1,21 @@
+/*!
+ * j+,(c) zhng(zhangwen77@qq.com)
+ */
+
+/*
+//≤‚ ‘
+//<div id="container"><a href="http://www.baidu.com" alt="dubai">dubai</a></div>
+var divElement = document.createElement( "div" );
+var aElement = document.createElement( "a" );
+var txtNode = document.createTextNode( "dubai" );
+
+divElement.id = "container";//divElement.setAttribute( "id", "container" );
+aElement.href = "http://www.baidu.com";
+aElement.setAttribute( "alt", "dubai");
+
+aElement.appendChild( txtNode );
+divElement.appendChild( aElement );
+document.body.appendChild( divElement );
+*/
+var j = {};
+//utils
